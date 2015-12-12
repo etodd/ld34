4a59aa62cc4c58d43d0412e2209191f10355fd81
BenjaminHoman <o0o089@yahoo.com>
2015-12-11 22:10:52 -0500

    removed spaced!

--- app/app.js
+++ app/app.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function (req, res){
+	res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+var server = app.listen(3000, function (){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
 });
