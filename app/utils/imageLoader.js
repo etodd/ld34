8afd13431cca3ec37add05ca2c0e1a28bae09379
BenjaminHoman <o0o089@yahoo.com>
2015-12-13 13:51:42 -0500

    there are now two other cells, obstacles or stairs to levels

--- app/utils/imageLoader.js
+++ app/utils/imageLoader.js
@@ -1,12 +1,9 @@
 var getPixels = require("get-pixels")
 
-const STAIR_CONNECTOR = -1;
-const OBSTICLE = -2;
-const VERT_LINES = -3;
-const ROCKS = -4;
-const TREES = -5;
-const SPAWN_LOCATION = -6;
-const RANDOM_SPAWN = -7;
+const STAIR_CONNECTOR_LVL_1 = -2;
+const STAIR_CONNECTOR_LVL_2 = -3;
+//ect...
+const OBSTICLE = -1;
 var Pixel = function(r,g,b,a){
 	this.r = r;
 	this.g = g;
@@ -18,29 +15,11 @@ Pixel.prototype.getType = function(){
 	if (this.a == 0)
 		return 0;
 
-	if (this.r == 0 && this.g == 0 && this.b == 0){
-		return STAIR_CONNECTOR;
-
-	} else if (this.r == 255 && this.g == 255 && this.b == 255){
+	if (this.r == 255 && this.g == 255 && this.b == 255){
 		return OBSTICLE;
 
-	} else if (this.r == 255 && this.g == 0 && this.b == 246){
-		return VERT_LINES;
-
-	} else if (this.r == 255 && this.g == 0 && this.b == 0){
-		return ROCKS;
-
-	} else if (this.r == 30 && this.g == 255 && this.b == 0){
-		return TREES;
-
-	} else if (this.r == 0 && this.g == 54 && this.b == 255){
-		return SPAWN_LOCATION;
-
-	} else if (this.r == 0 && this.g == 54 && this.b == 255){
-		return RANDOM_SPAWN;
-
 	} else {
-		return 0;
+		return STAIR_CONNECTOR_LVL_1;
 	}
 };
 
